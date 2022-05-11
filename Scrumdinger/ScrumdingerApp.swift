a3951a2dac30272472987d3008c5ec94c9f1cfdc
NickCecc <nickcecchin36@gmail.com>
2022-05-10 21:34:00 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Nick Cecchin on 2022-05-10.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
